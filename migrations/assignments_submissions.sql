4934952914b2b6fbb80997076094fd228e5b91fd
malak <malak-y@live.com>
2020-01-06 22:22:59 +0000

    add 2_queries_joins

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -5,13 +5,13 @@ CREATE TABLE assignments
   content TEXT,
   day INTEGER,
   chapter INTEGER,
-  durtion INTEGER
+  duration INTEGER
 );
 CREATE TABLE assignment_submissions
 (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duration INTEGER,
-  submission_date DATE
+  submission_date DATE,
+  duration INTEGER
 );
